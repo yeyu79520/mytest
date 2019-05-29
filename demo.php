0fb259bbacf5a62541670e25171bbe7314ca8d7b
zhongpeng79520 <zhongpeng79520@126.com>
2019-05-29 13:22:46 +0800

    提交

---
+++ demo.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/29 0029
+ * Time: 11:57
+ */
+require_once  "./vendor/autoload.php";
+use Valid\Tool;
+
+Tool::Valid();
---
+++ src/Valid/Tool.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/28 0028
+ * Time: 18:51
+ */
+
+namespace Valid;
+
+
+class Tool
+{
+
+     static function  Valid(){
+
+        echo  "hello";
+    }
+
+}
